f4ef53028eb4495c9a3c4df925aeedeb1c9a1bb5
7MinutesDead-Git <7minutesdead@gmail.com>
2022-08-30 00:39:16 -0500

    Add logout route

--- routes/auth.js
+++ routes/auth.js
@@ -2,15 +2,15 @@ const express = require('express')
 const passport = require("passport")
 const router = express.Router()
 
+// -----------------------------------------------------
 // @desc Authenticate with Google
 // @route GET /auth/google
 // Using GoogleStrategy to authenticate, created in config/passport.js.
-router.get('/google', passport.authenticate('google', {
-    // What is scope? https://developers.google.com/identity/protocols/googlescopes
-    // So this is saying to use whatever scope is included in the profile.
-    scope: ['profile']
-}))
+// What is scope? https://developers.google.com/identity/protocols/googlescopes
+// This is saying to use whatever scope is included in the profile.
+router.get('/google', passport.authenticate('google', {scope: ['profile']}))
 
+// -----------------------------------------------------
 // @desc Google authentication callback
 // @route GET /auth/google/callback
 router.get('/google/callback',
@@ -21,11 +21,16 @@ router.get('/google/callback',
         res.redirect('/dashboard')
     })
 
-// OR:
-// router.get('/google/callback',
-//     passport.authenticate('google', {
-//         successRedirect: '/dashboard',
-//         failureRedirect: '/'
-//     }))
+// -----------------------------------------------------
+// @desc Logout
+// @route GET /auth/logout
+router.get('/logout', (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err)
+        }
+        res.redirect('/')
+    })
+})
 
 module.exports = router
