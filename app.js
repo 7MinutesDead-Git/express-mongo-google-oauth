5a50954f6356e821d75ce4f38d6a82be19dc67a1
7MinutesDead-Git <7minutesdead@gmail.com>
2022-08-30 00:32:33 -0500

    Refactor app config and startup for better organization (...) - Break apart required order of operations based on functional scope, for clarity.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const hb = require('express-handlebars')
+// Needed to be able to use passport sessions.
 const session = require('express-session')
 // https://www.passportjs.org/packages/passport-google-oauth20/
 const passport = require('passport')
@@ -13,7 +14,6 @@ const passport = require('passport')
 // Set our environment variables based on the config files we created.
 dotenv.config({ path: './config/config.env' })
 const PORT = process.env.PORT || 6900
-const app = express()
 
 // -----------------------------------------------------
 // Configs
@@ -23,47 +23,58 @@ require('./config/passport')(passport)
 
 
 // -----------------------------------------------------
-// Middleware setup if we are running in development, ie "npm run dev".
-// Check package.json for the full command as it does set the environment variable too,
-// using cross-env package.
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'))
+const app = express()
+
+function setupMiddleware() {
+    // Middleware setup if we are running in development, ie "npm run dev".
+    // Check package.json for the full command as it does set the environment variable too,
+    // using cross-env package.
+    if (process.env.NODE_ENV === 'development') {
+        app.use(morgan('dev'))
+    }
+    // Static folder for serving static files.
+    app.use(express.static('public'))
 }
-// Static folder for serving static files.
-app.use(express.static('public'))
 
-// Setting the view engine to handlebars, and changing the extension name to .hbs
-// because .handlebars is a mouthful of a file name.
-app.engine('.hbs', hb.engine({
-    extname: '.hbs',
-    defaultLayout: 'main'
-}))
-app.set('view engine', '.hbs')
-app.set('views', './views')
+function setupEngine() {
+    // Setting the view engine to handlebars, and changing the extension name to .hbs
+    // because .handlebars is a mouthful of a file name.
+    app.engine('.hbs', hb.engine({
+        extname: '.hbs',
+        defaultLayout: 'main'
+    }))
+    app.set('view engine', '.hbs')
+    app.set('views', './views')
+}
 
-// Session middleware (needs to be loaded before Passport)
-// resave: false => Don't save a session if nothing is modified.
-// saveUninitialized: false => Don't create a session until something is stored.
-app.use(session({
-    secret: process.env.KEYBOARD_CAT_SESSION,
-    resave: false,
-    saveUninitialized: false
-}))
+function setupSession() {
+    // Session middleware (needs to be loaded before Passport)
+    // resave: false => Don't save a session if nothing is modified.
+    // saveUninitialized: false => Don't create a session until something is stored.
+    app.use(session({
+        secret: process.env.KEYBOARD_CAT_SESSION,
+        resave: false,
+        saveUninitialized: false
+    }))
+    // Passport middleware
+    app.use(passport.initialize(undefined))
+    app.use(passport.session(undefined))
+}
 
-// Passport middleware
-// TODO: Not sure if this needs to be callbacks or invoked right away.
-app.use(passport.initialize)
-app.use(passport.session)
+function setupRoutes() {
+    // NOTE: Remember to export the router from the routes/index.js file, since we're
+    // requiring it here.
+    app.use('/', require('./routes/index'))
+    app.use('/auth', require('./routes/auth'))
+}
 
 // -----------------------------------------------------
-// Routes
-// NOTE: Remember to export the router from the routes/index.js file, since we're
-// requiring it here.
-app.use('/', require('./routes/index'))
-app.use('/auth', require('./routes/auth'))
-
-// -----------------------------------------------------
+// Start here
+setupMiddleware()
+setupEngine()
+setupSession()
+setupRoutes()
 app.listen(PORT, async () => {
-    console.log(`🐡 Node up as ${process.env.NODE_ENV} on port ${PORT} 🐡`)
     await connectDB()
+    console.log(`🐡 Node up as ${process.env.NODE_ENV} on port ${PORT} 🐡`)
 })
